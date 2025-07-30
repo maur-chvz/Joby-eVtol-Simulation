faedd1f9ac7ef9051da32e95b7fa833f447a003c
Mauricio Chavez <mauricioch010202@gmail.com>
2025-07-30 13:19:55 -0400

    added description to config.h

--- src/config.h
+++ src/config.h
@@ -1,6 +1,15 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/**
+ * @brief Simulation configurations
+ * 
+ * TIME_STEP: simulation time step (h)
+ * DURATION: simulation total time (h)
+ * CHARGERS: total number of chargers
+ * VEHICLES: total number of vehicles
+ * COMPANY_CONFIGURATIONS: total number company types
+ */
 namespace Config {
     static constexpr double TIME_STEP = 1.0 / 3600;
     static constexpr double DURATION = 3.0;
